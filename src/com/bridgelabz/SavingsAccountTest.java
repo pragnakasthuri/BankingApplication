f536ec684ac24d73fda267d42be60879da4ece9e
pragnakasthruri <kpragna25@gmail.com>
2022-03-08 10:16:40 +0530

    Adding the code for Banking Application

--- src/com/bridgelabz/SavingsAccountTest.java
+++ src/com/bridgelabz/SavingsAccountTest.java
@@ -13,74 +13,61 @@ public class SavingsAccountTest {
     public static void main(String[] args) {
 
         /**
-         * 1.Creating new object for saver1
+         * 1.Creating new object for saver1 and saver2
          */
         SavingsAccount saver1 = new SavingsAccount();
+        SavingsAccount saver2 = new SavingsAccount();
+
         /**
-         * 2.Setting savingsBalance to 2000
+         * 2.Setting savingsBalance for saver1 to 2000 and saver2 to 3000
          */
         saver1.setSavingsBalance(2000);
+        saver2.setSavingsBalance(3000);
+
         /**
-         * 3.Taking interest rate as 4%
+         * 3.Modifying interest rate as 4% to both saver1 and saver2
          */
         saver1.modifyInterestRate(4);
+        saver2.modifyInterestRate(4);
+
         /**
-         * 5.Calculating the MonthlyInterest
+         * 4.Calculating the MonthlyInterest for saver1 and saver2
          */
         saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
+
         /**
-         * 6.printing the savingsBalance of saver1 with 4% interest
+         * 5.printing the savingsBalance of saver1 and saver2 with 4% interest
          */
         System.out.println("Saver1: New Balance with 4% interest rate: "+saver1.getSavingsBalance());
-
-
-        /**
-         * 7.Creating new object for saver2
-         */
-        SavingsAccount saver2 = new SavingsAccount();
-        /**
-         * 8.Setting savingsBalance to 3000
-         */
-        saver2.setSavingsBalance(3000.00);
-        /**
-         * 9.Taking interest rate as 4%
-         */
-        saver2.modifyInterestRate(4);
-        /**
-         * 10.Calculating the MonthlyInterest
-         */
-        saver2.calculateMonthlyInterest();
-        /**
-         * 11.printing the savingsBalance of saver2 with 4% interest
-         */
         System.out.println("Saver2: New Balance with 4% interest rate: "+saver2.getSavingsBalance());
         System.out.println();
 
 
         /**
-         * 12.Taking interest rate as 5%
+         * 6.modifying interest rate as 5% to saver1
          */
         saver1.modifyInterestRate(5);
         /**
-         * 13.Calculating the MonthlyInterest
+         * 7.Calculating the MonthlyInterest for saver1
          */
         saver1.calculateMonthlyInterest();
         /**
-         * 14.printing the savingsBalance of saver1 with 5% interest
+         * 8.printing the savingsBalance of saver1 with 5% interest
          */
         System.out.println("Saver1: New Balance with 5% interest rate: "+saver1.getSavingsBalance());
 
 
         /**
-         * 15.Taking interest rate as 5%
+         * 9.modifying interest rate as 5% to saver2
          */
         saver2.modifyInterestRate(5);
         /**
-         * 16.Calculating the MonthlyInterest
+         * 10.Calculating the MonthlyInterest for saver2
          */
         saver2.calculateMonthlyInterest();
         /**
-         * printing the savingsBalance of saver2 with 5% interest
+         * 11.printing the savingsBalance of saver2 with 5% interest
          */
         System.out.println("Saver2: New Balance with 5% interest rate: "+saver2.getSavingsBalance());
     }
